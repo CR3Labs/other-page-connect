f824c2ba2b5d494810ad124bb3384fc6616d108e
Ryan Clements <ryanclementshax@gmail.com>
2024-05-17 08:54:15 -0400

    fix: address styles
    - set balance font weight to 600
    - set eth address separator to the proper unicode ····

--- packages/connectkit/src/utils/index.ts
+++ packages/connectkit/src/utils/index.ts
@@ -3,7 +3,7 @@ import { detect } from 'detect-browser';
 
 const truncateRegex = /^(0x[a-zA-Z0-9]{4})[a-zA-Z0-9]+([a-zA-Z0-9]{4})$/;
 
-const truncateEthAddress = (address?: string, separator: string = '••••') => {
+const truncateEthAddress = (address?: string, separator: string = '····') => {
   if (!address) return '';
   const match = address.match(truncateRegex);
   if (!match) return address;
