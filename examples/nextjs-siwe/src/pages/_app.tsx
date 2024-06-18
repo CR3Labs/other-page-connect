9616f4bc87138735043c58891ba3e8197c7cd315
0xm1kr <60800753+0xm1kr@users.noreply.github.com>
2024-06-17 18:19:42 -0600

    chore: add peer dependencies to siwe example

--- examples/nextjs-siwe/src/pages/_app.tsx
+++ examples/nextjs-siwe/src/pages/_app.tsx
@@ -3,6 +3,7 @@ import { siweClient } from '@/utils/siweClient';
 import { OPConnectProvider, getDefaultConfig } from 'opconnect';
 import type { AppProps } from 'next/app';
 import { WagmiProvider, createConfig } from 'wagmi';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const config = createConfig(
   getDefaultConfig({
@@ -11,14 +12,18 @@ const config = createConfig(
   })
 );
 
+const queryClient = new QueryClient();
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <WagmiProvider config={config}>
-      <siweClient.Provider>
-        <OPConnectProvider>
-          <Component {...pageProps} />
-        </OPConnectProvider>
-      </siweClient.Provider>
+       <QueryClientProvider client={queryClient}>
+        <siweClient.Provider>
+          <OPConnectProvider>
+            <Component {...pageProps} />
+          </OPConnectProvider>
+        </siweClient.Provider>
+      </QueryClientProvider>
     </WagmiProvider>
   );
 }
