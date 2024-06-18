c6c3b0ef8e86b8e688e872d3b015abd9c0f06bcd
Ty-Sir <tylersiravo@gmail.com>
2024-06-17 20:43:46 -0400

    query provider

--- examples/nextjs-siwe/src/pages/_app.tsx
+++ examples/nextjs-siwe/src/pages/_app.tsx
@@ -3,22 +3,27 @@ import { siweClient } from '@/utils/siweClient';
 import { OPConnectProvider, getDefaultConfig } from 'opconnect';
 import type { AppProps } from 'next/app';
 import { WagmiProvider, createConfig } from 'wagmi';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const config = createConfig(
   getDefaultConfig({
     walletConnectProjectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID!,
     appName: 'My OPConnect App',
+    ssr: true,
   })
 );
+const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <WagmiProvider config={config}>
-      <siweClient.Provider>
-        <OPConnectProvider>
-          <Component {...pageProps} />
-        </OPConnectProvider>
-      </siweClient.Provider>
+      <QueryClientProvider client={queryClient}>
+        <siweClient.Provider>
+          <OPConnectProvider>
+            <Component {...pageProps} />
+          </OPConnectProvider>
+        </siweClient.Provider>
+      </QueryClientProvider>
     </WagmiProvider>
   );
 }
